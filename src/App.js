7812556be698821552b40660f2072ffc6453d523
whitewoodww <whitewoodww.gmail.com>
2026-06-13 15:40:26 -0300

    alternando os estágios do jogo

--- src/App.js
+++ src/App.js
@@ -22,12 +22,27 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words, setWords] = useState(wordsList);
 
-  console.log(words)
+
+  // Start game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  // process letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // restart the game
+ const retry = () => {
+  setGameStage(stages[0].name)
+ }
+
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
 }
